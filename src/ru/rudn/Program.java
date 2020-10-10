5f72ac7e255596f948256593cc8310bc2d769d6e
Your Name <you@example.com>
2020-10-10 18:05:25 +0300

    Округление и форматный вывод

--- src/ru/rudn/Program.java
+++ src/ru/rudn/Program.java
@@ -1,13 +1,26 @@
 package ru.rudn;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Program {
     static Scanner scanner;
     public static void main(String[] args) {
-        scanner = new Scanner(System.in);
-        countSum(enterArray(scanner.nextInt(),scanner.nextInt()));
+        //scanner = new Scanner(System.in);
+        //countSum(enterArray(scanner.nextInt(),scanner.nextInt()));
+        double y = 6.67676;
+        DecimalFormat decimalFormat = new DecimalFormat(".###");
+
+        System.out.println(decimalFormat.format(y));
+
+        //System.out.println(9);
+        //System.out.printf("%.2f",5.6767);
+
+        // Math.ceil() // в большую сторону
+        // Math.round() // математически
+        // Math.floor() // округл в меньшую сторону
+
     }
 
     static int[][] enterArray(int x, int y) {
